6a90acba0193c4d7ed1a9db7468415cee60cfcf8
Julian Cadieux <julian.cadieux@stu.ocsb.ca>
2025-04-09 11:14:34 -0400

    added jumping and gravity

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
  * 
  * 17 - Canvas Animation
  * 
- * Author:
+ * Author: Julian Cadieux
  * 
  */
 
@@ -13,7 +13,7 @@ import Player from "./player.js";
 import { CANVAS, CTX, MS_PER_FRAME, KEYS } from "./globals.js";
 
 // Globals
-const HERO = new Player(20, 50, 48, 48);
+const HERO = new Player(20, 90, 48, 48);
 
 let frame_time = performance.now()
 
@@ -30,6 +30,9 @@ document.addEventListener("contextmenu", (event) => {
  * The user pressed a key on the keyboard 
  */
 function keypress(event) {
+  if (event.keyCode == KEYS.SPACE || event.keyCode == KEYS.W || event.keyCode == KEYS.UP_ARROW) {
+    HERO.jump()
+  }
 
 }
 
--- js/player.js
+++ js/player.js
@@ -5,7 +5,7 @@
  * 
  * Acts as a sprite or "hero" for the game
  * 
- * Author: 
+ * Author: Julian Cadieux 
  */
 
 import { CTX, CANVAS, GRAVITY, FLOOR } from "./globals.js"
@@ -26,9 +26,28 @@ export default class Player {
   }
 
   /**
+   * For if object is touching anything 
+   */
+  get right() { return this.position.x + this.width }
+  get bottom() { return this.position.y + this.height }
+  get left() { return this.position.x }
+  get top() { return this.position.y }
+
+  /**
    * Main function to update location, velocity, and image
    */
   update() {
+    // add gravity to hero
+    this.velocity.y += GRAVITY
+
+    // if we hit the floor, we stop falling
+    if (this.bottom >FLOOR) {
+      this.velocity.y = 0
+      this.position.y = FLOOR - this.height
+    }
+    // update the location of hero
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
     this.draw();
   }
 
@@ -39,4 +58,12 @@ export default class Player {
     CTX.fillStyle = "yellow";
     CTX.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  jump() {
+    this.position.y -= 2
+    this.velocity.y = -20
+    
+  }
+
 }
+
